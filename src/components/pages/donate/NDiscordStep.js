91f584168f79f2ab49f59d2edcffa86af043befb
neil.rahmouni@gmail.com <neil.rahmouni@gmail.com>
2020-06-19 23:56:47 +0200

    Splitted NDiscordStep & NCheckoutStep

---
+++ src/components/pages/donate/NDiscordStep.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { useTranslation } from 'react-i18next';
+
+import { Collapse, Grid, TextField, InputAdornment } from '@material-ui/core';
+import { Alert } from '@material-ui/lab';
+
+export function NDiscordStep(props) {
+    const { t } = useTranslation();
+    const classes = useStyles();
+
+    return (
+        <>
+            <Collapse in={props.username===''||props.tag.length<4}>
+                <Alert severity="warning" className={classes.alert}>
+                    {t('RahNeil_N3.Irus.Donations.Discord.Warning.0')}
+                    &nbsp;
+                    <strong>
+                        {t('RahNeil_N3.Irus.Donations.Discord.Warning.1')}
+                    </strong>
+                    &nbsp;
+                    {t('RahNeil_N3.Irus.Donations.Discord.Warning.2')}
+                    &nbsp;
+                    <strong>
+                        {t('RahNeil_N3.Irus.Donations.Discord.Warning.3')}
+                    </strong>
+                    &nbsp;
+                    {t('RahNeil_N3.Irus.Donations.Discord.Warning.4')}
+                </Alert>
+            </Collapse>
+
+            <Collapse in={props.username!==''&&props.tag.length===4}>
+                <Alert severity="info" className={classes.alert}>
+                    {t('RahNeil_N3.Irus.Donations.Discord.Info.0')}
+                    &nbsp;
+                    <strong>
+                        {t('RahNeil_N3.Irus.Donations.Discord.Info.1')}
+                    </strong>
+                    &nbsp;
+                    {t('RahNeil_N3.Irus.Donations.Discord.Info.2')}
+                </Alert>
+            </Collapse>
+
+            <Grid container>
+                <Grid item>
+                    <TextField
+                        label={t('RahNeil_N3.Irus.Donations.Discord.Username')}
+                        value={props.username}
+                        onChange={(event) => props.setUsername(event.target.value)}
+                    />
+                </Grid>
+                <Grid item>
+                    <TextField
+                        label=' '
+                        placeholder='1234'
+                        value={props.tag}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                #
+                                </InputAdornment>
+                            ),
+                        }}
+                        style={{width: '64px'}}
+                        onChange={(event) => {
+                            if ((event.target.value.length<=4 && /^\d+$/.test(event.target.value))
+                                ||event.target.value==='') props.setTag(event.target.value);
+                        }}
+                    />
+                </Grid>
+            </Grid>
+        </>
+    );
+}
+
+const useStyles = makeStyles((theme) => ({
+    alert: {
+        marginBottom: theme.spacing(2),
+    },
+}));
--- src/components/pages/donate/NDonateModal.js
+++ src/components/pages/donate/NDonateModal.js
@@ -2,15 +2,15 @@ import React, { Suspense } from 'react';
 import { useTranslation } from 'react-i18next';
 import { makeStyles } from '@material-ui/core/styles';
 import { useHistory } from "react-router-dom";
-import { PayPalButton } from "react-paypal-button-v2";
-import { useSnackbar } from 'notistack';
 
-import { Dialog, TextField, Grid, DialogTitle, Fade, InputAdornment, Collapse, DialogActions, Slide, Button, DialogContent, useTheme, useMediaQuery, IconButton, Typography } from '@material-ui/core';
+import { Dialog, DialogTitle, Fade, InputAdornment, Collapse, DialogActions, Slide, Button, DialogContent, useTheme, useMediaQuery, IconButton, Typography } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
-import { Skeleton, Alert } from '@material-ui/lab';
+import { Skeleton } from '@material-ui/lab';
 
 import { NStepper, NStepperLoading } from '../../NStepper.js';
 import { NMinecraftStep, NMinecraftStepLoading } from './NMinecraftStep.js';
+import { NDiscordStep } from './NDiscordStep.js';
+import { NCheckoutStep } from './NCheckoutStep.js';
 
 function Translation(props) {
     const { t } = useTranslation();
@@ -24,12 +24,13 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 
 export function NDonateModal(props) {
     const history = useHistory();
+    const fullScreen = useMediaQuery(useTheme().breakpoints.down('xs'));
+    const classes = useStyles();
+
     const [uuid, setUuid] = React.useState(null);
     const [discordUsername, setDiscordUsername] = React.useState('');
     const [discordTag, setDiscordTag] = React.useState('');
-    const fullScreen = useMediaQuery(useTheme().breakpoints.down('xs'));
-    const classes = useStyles();
-    const { enqueueSnackbar } = useSnackbar();
+
     const [success, setSuccess] = React.useState(false);
     const [successStep, setSuccessStep] = React.useState(0);
 
@@ -59,146 +60,49 @@ export function NDonateModal(props) {
             },
             content: {
                 translation: (
-                    <>
-                        <Collapse in={discordUsername===''||discordTag.length<4}>
-                            <Alert severity="warning" className={classes.alert}>
-                                <Translation t='RahNeil_N3.Irus.Donations.Discord.Warning.0'/>
-                                &nbsp;
-                                <strong>
-                                    <Translation t='RahNeil_N3.Irus.Donations.Discord.Warning.1'/>
-                                </strong>
-                                &nbsp;
-                                <Translation t='RahNeil_N3.Irus.Donations.Discord.Warning.2'/>
-                                &nbsp;
-                                <strong>
-                                    <Translation t='RahNeil_N3.Irus.Donations.Discord.Warning.3'/>
-                                </strong>
-                                &nbsp;
-                                <Translation t='RahNeil_N3.Irus.Donations.Discord.Warning.4'/>
-                            </Alert>
-                        </Collapse>
-
-                        <Collapse in={discordUsername!==''&&discordTag.length===4}>
-                            <Alert severity="info" className={classes.alert}>
-                                <Translation t='RahNeil_N3.Irus.Donations.Discord.Info.0'/>
-                                &nbsp;
-                                <strong>
-                                    <Translation t='RahNeil_N3.Irus.Donations.Discord.Info.1'/>
-                                </strong>
-                                &nbsp;
-                                <Translation t='RahNeil_N3.Irus.Donations.Discord.Info.2'/>
-                            </Alert>
-                        </Collapse>
-
-                        <Grid container>
-                            <Grid item>
-                                <TextField
-                                    label={
-                                        <Translation t='RahNeil_N3.Irus.Donations.Discord.Username'/>
-                                    }
-                                    value={discordUsername}
-                                    onChange={(event) => setDiscordUsername(event.target.value)}
-                                />
-                            </Grid>
-                            <Grid item>
-                                <TextField
-                                    label=' '
-                                    placeholder='1234'
-                                    value={discordTag}
-                                    InputProps={{
-                                        startAdornment: (
-                                          <InputAdornment position="start">
-                                            #
-                                          </InputAdornment>
-                                        ),
-                                    }}
-                                    style={{width: '64px'}}
-                                    onChange={(event) => {
-                                        if ((event.target.value.length<=4 && /^\d+$/.test(event.target.value))
-                                            ||event.target.value==='') setDiscordTag(event.target.value);
-                                    }}
-                                />
-                            </Grid>
-                        </Grid>
-                    </>
+                    <NDiscordStep
+                        username={discordUsername}
+                        setUsername={setDiscordUsername}
+                        tag={discordTag}
+                        setTag={setDiscordTag}
+                    />
                 ),
             }
         },
         {
             label: {
-                placeholder: "Choose amount & checkout",
+                placeholder: "Checkout",
                 translation: "RahNeil_N3.Irus.Donations.Checkout.Title",
             },
             content: {
                 translation: (
-                    <PayPalButton
-                        createOrder={function(data, actions) {
-                            return fetch('/api/createOrder/5.00', {
-                                method: 'post'
-                            }).then(function(res) {
-                                return res.json();
-                            }).then(function(data) {
-                                return data.id;
-                            });
-                        }}
-                        onApprove={function(data, actions) {
-                            return fetch('/api/approveOrder/'+data.orderID+'/'
-                                +(discordUsername===''?'null':discordUsername)
-                                +'/'+(discordTag===''?'null':discordTag),
-                            {
-                                method: 'post'
-                            }).then(function(res) {
-                                return res.json();
-                            }).then(function(data) {
-                                if (data.status==='success') {
-                                    setSuccess(true);
+                    <NCheckoutStep
+                        discordUsername={discordUsername}
+                        discordTag={discordTag}
+                        onSuccess={() => {
+                            setSuccess(true);
 
-                                    const timeoutID = window.setTimeout(() => {
-                                        setSuccessStep(1);
-                                        window.clearTimeout(timeoutID);
-                                    }, 500);
-                                    const timeoutID2 = window.setTimeout(() => {
-                                        setSuccessStep(2);
-                                        window.clearTimeout(timeoutID2);
-                                    }, 1750);
-                                    const timeoutID3 = window.setTimeout(() => {
-                                        setSuccessStep(3);
-                                        window.clearTimeout(timeoutID3);
-                                    }, 2250);
-                                    const timeoutID4 = window.setTimeout(() => {
-                                        history.push('/');
-                                        window.clearTimeout(timeoutID4);
-                                    }, 5000);
-                                    const timeoutID5 = window.setTimeout(() => {
-                                        setSuccess(false);
-                                        setSuccessStep(0);
-                                        window.clearTimeout(timeoutID5);
-                                    }, 5250);
-                                }else{
-                                    enqueueSnackbar(
-                                        <Translation t='RahNeil_N3.Irus.Error.General' />
-                                    , {variant: 'error'});
-                                    history.push('/');
-                                }
-                            });
-                        }}
-                        catchError={(error) => {
-                            enqueueSnackbar(error, {variant: 'error'});
-                            history.push('/');
-                        }}
-                        onError={() => {
-                            enqueueSnackbar(
-                                <Translation t='RahNeil_N3.Irus.Error.General' />
-                            , {variant: 'error'});
-                            history.push('/');
-                        }}
-                        onCancel={() => {
-                            enqueueSnackbar(
-                                <Translation t='RahNeil_N3.Irus.Donations.Checkout.Snackbar.Cancelled' />
-                            , {variant: 'warning'});
-                        }}
-                        options={{
-                            clientId: "Aem1NU0lE5_WzawW0TOiHCj4RhxBWlbuR-oEv7khOF_m86E7hUpOhOtO8ioY_LYNMu61VJsAShTUxdSd"
+                            const timeoutID = window.setTimeout(() => {
+                                setSuccessStep(1);
+                                window.clearTimeout(timeoutID);
+                            }, 500);
+                            const timeoutID2 = window.setTimeout(() => {
+                                setSuccessStep(2);
+                                window.clearTimeout(timeoutID2);
+                            }, 1750);
+                            const timeoutID3 = window.setTimeout(() => {
+                                setSuccessStep(3);
+                                window.clearTimeout(timeoutID3);
+                            }, 2250);
+                            const timeoutID4 = window.setTimeout(() => {
+                                history.push('/');
+                                window.clearTimeout(timeoutID4);
+                            }, 5000);
+                            const timeoutID5 = window.setTimeout(() => {
+                                setSuccess(false);
+                                setSuccessStep(0);
+                                window.clearTimeout(timeoutID5);
+                            }, 5250);
                         }}
                     />
                 ),
