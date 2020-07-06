8fa3cdd13013c36ee401706197fa80a4430bcf08
neil.rahmouni@gmail.com <neil.rahmouni@gmail.com>
2020-07-06 13:00:14 +0200

    Fixed a loooot of random things & bugs

--- src/components/pages/home/NDonatorCard.js
+++ src/components/pages/home/NDonatorCard.js
@@ -2,11 +2,51 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 
-import { CardContent, CardMedia, Typography, Grid, Tooltip, SvgIcon, useMediaQuery, theme } from '@material-ui/core';
+import { CardContent, CardMedia, Typography, Grid, Tooltip, SvgIcon, useMediaQuery } from '@material-ui/core';
 import { Skeleton } from '@material-ui/lab';
 import HistoryIcon from '@material-ui/icons/History';
 
 import { NCard } from '../../NCard';
+
+function CrownIcon() {
+    const classes = useStyles();
+
+    return (
+        <SvgIcon color="action" className={classes.smallIcon}>
+            <path fill="currentColor" d="M5 16L3 5L8.5 10L12 4L15.5 10L21 5L19 16H5M19 19C19 19.6 18.6 20 18 20H6C5.4 20 5 19.6 5 19V18H19V19Z" />
+        </SvgIcon>
+    );
+}
+
+function PodiumTopIcon() {
+    const classes = useStyles();
+
+    return (
+        <SvgIcon color="action" className={classes.podium}>
+            <path fill="currentColor" d="M12,7.09L14.45,8.58L13.8,5.77L16,3.89L13.11,3.64L12,1L10.87,3.64L8,3.89L10.18,5.77L9.5,8.58L12,7.09M15,23H9V10H15V23M1,17V23H7V17H1M5,21H3V19H5V21M17,13V23H23V13H17M21,21H19V15H21V21Z" />
+        </SvgIcon>
+    );
+}
+
+function PodiumSecondIcon() {
+    const classes = useStyles();
+
+    return (
+        <SvgIcon color="action" className={classes.podium}>
+            <path fill="currentColor" d="M20,10.09L22.45,11.58L21.8,8.77L24,6.89L21.11,6.64L20,4L18.87,6.64L16,6.89L18.18,8.77L17.5,11.58L20,10.09M23,23H17V13H23V23M1,17V23H7V17H1M5,21H3V19H5V21M9,10V23H15V10H9M13,21H11V12H13V21Z" />
+        </SvgIcon>
+    );
+}
+
+function PodiumThirdIcon() {
+    const classes = useStyles();
+
+    return (
+        <SvgIcon color="action" className={classes.podium}>
+            <path fill="currentColor" d="M4,13.09L6.45,14.58L5.8,11.77L8,9.89L5.11,9.64L4,7L2.87,9.64L0,9.89L2.18,11.77L1.5,14.58L4,13.09M7,23H1V17H7V23M9,10V23H15V10H9M13,21H11V12H13V21M17,13V23H23V13H17M21,21H19V15H21V21Z" />
+        </SvgIcon>
+    );
+}
 
 export function NDonatorCard(props) {
     const { t } = useTranslation();
@@ -19,33 +59,16 @@ export function NDonatorCard(props) {
     ];
     const labelFromCurrencyCode = (code) => currencies.find(c => c.value === code).label;
 
-
     function NDonatorCardIconLabel() {
         return (
             <>
                 <Grid item className={classes.icon}>
-                    {props.latest?<HistoryIcon color="action" />:null}
-                    {props.top?
-                        <SvgIcon color="action">
-                            {isScreenLarge?
-                                <path fill="currentColor" d="M12,7.09L14.45,8.58L13.8,5.77L16,3.89L13.11,3.64L12,1L10.87,3.64L8,3.89L10.18,5.77L9.5,8.58L12,7.09M15,23H9V10H15V23M1,17V23H7V17H1M5,21H3V19H5V21M17,13V23H23V13H17M21,21H19V15H21V21Z" />
-                            :
-                                <path fill="currentColor" d="M5 16L3 5L8.5 10L12 4L15.5 10L21 5L19 16H5M19 19C19 19.6 18.6 20 18 20H6C5.4 20 5 19.6 5 19V18H19V19Z" />
-                            }
-                        </SvgIcon>
-                    :null}
-                    {props.second?
-                        <SvgIcon color="action" className={classes.podium}>
-                            <path fill="currentColor" d="M20,10.09L22.45,11.58L21.8,8.77L24,6.89L21.11,6.64L20,4L18.87,6.64L16,6.89L18.18,8.77L17.5,11.58L20,10.09M23,23H17V13H23V23M1,17V23H7V17H1M5,21H3V19H5V21M9,10V23H15V10H9M13,21H11V12H13V21Z" />
-                        </SvgIcon>
-                    :null}
-                    {props.third?
-                        <SvgIcon color="action" className={classes.podium}>
-                            <path fill="currentColor" d="M4,13.09L6.45,14.58L5.8,11.77L8,9.89L5.11,9.64L4,7L2.87,9.64L0,9.89L2.18,11.77L1.5,14.58L4,13.09M7,23H1V17H7V23M9,10V23H15V10H9M13,21H11V12H13V21M17,13V23H23V13H17M21,21H19V15H21V21Z" />
-                        </SvgIcon>
-                    :null}
+                    {props.latest?<HistoryIcon className={classes.smallIcon} color="action" />:null}
+                    {props.top?isScreenLarge?<PodiumTopIcon/>:<CrownIcon/>:null}
+                    {props.second?<PodiumSecondIcon/>:null}
+                    {props.third?<PodiumThirdIcon/>:null}
                 </Grid>
-                <Grid item>
+                <Grid item className={classes.labelWrap}>
                     <Typography variant="subtitle1" color="textSecondary" noWrap>
                         {props.latest?t('RahNeil_N3.Irus.Cards.Latest.Short'):null}
                         {props.top?t('RahNeil_N3.Irus.Cards.Top.Short'):null}
@@ -75,12 +98,12 @@ export function NDonatorCard(props) {
                 </Typography>
 
                 {longText===null?
-                    <Grid container wrap="nowrap">
+                    <Grid container alignItems='center' wrap='nowrap'>
                         <NDonatorCardIconLabel />
                     </Grid>
                 :
                     <Tooltip title={longText} arrow>
-                        <Grid container wrap="nowrap">
+                        <Grid container alignItems='center' wrap='nowrap'>
                             <NDonatorCardIconLabel />
                         </Grid>
                     </Tooltip>
@@ -99,6 +122,7 @@ export function NDonatorCard(props) {
 
 export function NDonatorCardLoading(props) {
     const classes = useStyles();
+    const isScreenLarge = useMediaQuery(useTheme().breakpoints.up("lg"));
 
     return (
         <NCard className={classes.root} isDarkTheme={props.isDarkTheme}>
@@ -119,22 +143,10 @@ export function NDonatorCardLoading(props) {
                 </Typography>
                 <Grid container>
                     <Grid item className={classes.icon}>
-                        {props.latest?<HistoryIcon color="action" />:null}
-                        {props.top?
-                            <SvgIcon color="action">
-                                <path fill="currentColor" d="M5 16L3 5L8.5 10L12 4L15.5 10L21 5L19 16H5M19 19C19 19.6 18.6 20 18 20H6C5.4 20 5 19.6 5 19V18H19V19Z" />
-                            </SvgIcon>
-                        :null}
-                        {props.second?
-                            <SvgIcon color="action" className={classes.podium}>
-                                <path fill="currentColor" d="M20,10.09L22.45,11.58L21.8,8.77L24,6.89L21.11,6.64L20,4L18.87,6.64L16,6.89L18.18,8.77L17.5,11.58L20,10.09M23,23H17V13H23V23M1,17V23H7V17H1M5,21H3V19H5V21M9,10V23H15V10H9M13,21H11V12H13V21Z" />
-                            </SvgIcon>
-                        :null}
-                        {props.third?
-                            <SvgIcon color="action" className={classes.podium}>
-                                <path fill="currentColor" d="M4,13.09L6.45,14.58L5.8,11.77L8,9.89L5.11,9.64L4,7L2.87,9.64L0,9.89L2.18,11.77L1.5,14.58L4,13.09M7,23H1V17H7V23M9,10V23H15V10H9M13,21H11V12H13V21M17,13V23H23V13H17M21,21H19V15H21V21Z" />
-                            </SvgIcon>
-                        :null}
+                        {props.latest?<HistoryIcon className={classes.smallIcon} color="action" />:null}
+                        {props.top?isScreenLarge?<PodiumTopIcon/>:<CrownIcon/>:null}
+                        {props.second?<PodiumSecondIcon/>:null}
+                        {props.third?<PodiumThirdIcon/>:null}
                     </Grid>
                     <Grid item>
                         <Typography variant="subtitle1" color="textSecondary">
@@ -165,17 +177,28 @@ const useStyles = makeStyles((theme) => ({
     },
     cover: {
         width: 151,
+        height: 151,
         backgroundPosition: 'top',
         backgroundSize: 'auto',
+        flexBasis: '50%',
     },
     content: {
         flex: 1,
+        flexBasis: '50%',
+        minWidth: 0,
     },
     icon: {
         paddingRight: theme.spacing(1),
     },
+    smallIcon: {
+        marginTop: '2px',
+        marginBottom: '-2px',
+    },
     podium: {
-        marginTop: '-4px',
-        marginBottom: '4px',
+        marginTop: '-2px',
+        marginBottom: '2px',
+    },
+    labelWrap: {
+        width: '85%',
     }
 }));
