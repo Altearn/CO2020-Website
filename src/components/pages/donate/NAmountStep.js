7949e74c37ec905ebacb01eda95063b29e5d2b4f
neil.rahmouni@gmail.com <neil.rahmouni@gmail.com>
2020-07-31 12:46:46 +0200

    Resolved #63

--- src/components/pages/donate/NAmountStep.js
+++ src/components/pages/donate/NAmountStep.js
@@ -1,69 +1,62 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Slider from '@material-ui/core/Slider';
-import Input from '@material-ui/core/Input';
-import TextField from "@material-ui/core/TextField";
-import MenuItem from "@material-ui/core/MenuItem";
+
+import { Grid, Slider, Input, Button, Dialog, DialogTitle, ListItem, ListItemText, List, Tooltip } from '@material-ui/core';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 
 import { currencies, getCurrencyLabel, hasCurrencyDecimals } from '../../NCurrencies';
 
 export function NAmountStep(props) {
-    
     const {t} = useTranslation();
     const classes = useStyles();
+    const [dialogOpened, setDialogOpened] = React.useState(false);
 
-    if(!props.currency)
-        props.setCurrency(t('RahNeil_N3.Irus.Currency.Default.Code'));
-
-    let currencyLabel = getCurrencyLabel(props.currency || 'USD');
-    const handleChangeCurrency = (event) => {
-        props.setCurrency(event.target.value);
-        currencyLabel = getCurrencyLabel(event.target.value);
-        updateMarks();
-    };
-
+    const handleChangeCurrency = (value) => {
+        props.setCurrency(value);
+        setDialogOpened(false);
+    }
     const handleSliderChange = (event, newValue) => props.setAmount(newValue);
 
     let marks = [];
-    const updateMarks = () => {
+    React.useEffect(() => {
         marks = ([1,5,10,20,30,40,50]
-            .map(n => ({ value: n, label: (t('RahNeil_N3.Irus.Currency.IsPlacedAfter')===true?'':currencyLabel)+n+(t('RahNeil_N3.Irus.Currency.IsPlacedAfter')===false?'':currencyLabel) })));
-    };
-    updateMarks();
+            .map(n => ({ value: n, label: (t('RahNeil_N3.Irus.Currency.IsPlacedAfter')?'':getCurrencyLabel(props.currency||t('RahNeil_N3.Irus.Currency.Default.Code')||'USD'))+n+(t('RahNeil_N3.Irus.Currency.IsPlacedAfter')===false?'':getCurrencyLabel(props.currency||t('RahNeil_N3.Irus.Currency.Default.Code')||'USD')) })));
+    }, [props.currency]);
 
     return (
         <div className={classes.root}>
             <Grid container direction="row" justify="space-between" alignItems="center" className={classes.selectors}>
-                <Grid item xs={6}>
+                <Grid item>
                     {t('RahNeil_N3.Irus.Donations.Amount.Label')}:
                 </Grid>
-                <Grid item xs={6}>
-                    <Input
-                        className={classes.amount}
-                        value={props.amount}
-                        onChange={(event) => {
-                            if (((hasCurrencyDecimals(props.currency||'USD')?/^\d+(|\.|,)\d{0,2}$/:/^\d+$/).test(event.target.value))||event.target.value==='')
-                                props.setAmount(event.target.value.split(',').join('.'));
-                        }}
-                        onBlur={(event) => {
-                            if (event.target.value==='') props.setAmount(5);
-                        }}
-                    />
-                    <TextField
-                        className={classes.currency}
-                        noValidate
-                        select
-                        value={props.currency}
-                        onChange={handleChangeCurrency}
-                    >
-                        {currencies.map((option) => (
-                            <MenuItem key={option.code} value={option.code}>
-                                {option.label+' – '+option.code}
-                            </MenuItem>
-                        ))}
-                    </TextField>
+                <Grid item>
+                    <Grid container direction={t('RahNeil_N3.Irus.Currency.IsPlacedAfter')?"row":"row-reverse"} spacing={1}>
+                        <Grid item>
+                            <Input
+                                className={classes.amount}
+                                value={props.amount}
+                                onChange={(event) => {
+                                    if (((hasCurrencyDecimals(props.currency||t('RahNeil_N3.Irus.Currency.Default.Code')||'USD')?/^\d+(|\.|,)\d{0,2}$/:/^\d+$/).test(event.target.value))||event.target.value==='')
+                                        props.setAmount(event.target.value.split(',').join('.'));
+                                }}
+                                onBlur={(event) => {
+                                    if (event.target.value==='') props.setAmount(5);
+                                }}
+                            />
+                        </Grid>
+                        <Grid item>
+                            <Tooltip title={t('RahNeil_N3.Irus.Currency.Description')} arrow enterDelay={750}>
+                                <Button
+                                    startIcon={t('RahNeil_N3.Irus.Currency.IsPlacedAfter')?null:<KeyboardArrowDownIcon/>}
+                                    endIcon={t('RahNeil_N3.Irus.Currency.IsPlacedAfter')?<KeyboardArrowDownIcon/>:null}
+                                    onClick={() => setDialogOpened(true)}
+                                >
+                                    {getCurrencyLabel(props.currency||t('RahNeil_N3.Irus.Currency.Default.Code')||'USD')}
+                                </Button>
+                            </Tooltip>
+                        </Grid>
+                    </Grid>
                 </Grid>
             </Grid>
             <Slider
@@ -75,6 +68,26 @@ export function NAmountStep(props) {
                 max={50}
                 className={classes.slider}
             />
+            <Dialog
+                onClose={() => setDialogOpened(false)}
+                aria-labelledby={t('RahNeil_N3.Irus.Currency.Description')}
+                scroll="body"
+                open={dialogOpened}
+            >
+                <DialogTitle id="dialogCurrencySelectorId">{t('RahNeil_N3.Irus.Currency.Title')}</DialogTitle>
+                <List>
+                    {currencies.map((option) => (
+                        <ListItem
+                            button
+                            onClick={() => handleChangeCurrency(option.code)}
+                            key={option.code}
+                            selected={option.code===(props.currency||t('RahNeil_N3.Irus.Currency.Default.Code')||'USD')}
+                        >
+                            <ListItemText primary={t('RahNeil_N3.Irus.Currency.Name.'+(option.code || 'USD'))+' ('+option.code+')'} />
+                        </ListItem>
+                    ))}
+                </List>
+            </Dialog>
         </div>
     );
 }
@@ -85,12 +98,6 @@ const useStyles = makeStyles((theme) => ({
     },
     selectors: {
         marginBottom: theme.spacing(1),
-    },
-    currency: {
-        width: '25%',
-    },
-    amount: {
-       width: '75%',
     },
     slider: {
        margin: theme.spacing(0, 2),
